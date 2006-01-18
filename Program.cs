fa2ddd38b09bc92d6b444ee773d587173023fbaa
Aidan Fitzpatrick <aidan@reincubate.com>
2006-01-18 17:55:47 +0000

    Updated to handle unhandled exceptions

--- Program.cs
+++ Program.cs
@@ -10,13 +10,21 @@ namespace iTunesPauser {
 		/// The main entry point for the application.
 		/// </summary>
 		static void Main() {
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler( UnhandledException );
 			iTunesApp application = new iTunesApp();
 			
 			if( application.PlayerState == ITPlayerState.ITPlayerStateStopped )
 				application.Play();
 			else
 				application.Pause();
-
+		}
+		
+		private static void UnhandledException( Object sender, UnhandledExceptionEventArgs e ) { 
+			// Likely exceptions will stem from the Interop object,
+			// although given changes in its version there are a
+			// variety of cryptic causes. Frankly, we're best off
+			// failing silently and terminating, so our unhandled
+			// exception goes... largely unhandled.
 		}
 	}
 }
